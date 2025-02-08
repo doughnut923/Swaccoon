8dc15a14335649964db96f5480ecb326c4e5bd9d
Alysha Van Bibber <vanbibbe@ualberta.ca>
2025-02-08 10:56:35 -0700

    restoring files

---
+++ CMPUT 250 Base Unity Project/Assets/TechDemo/Scripts/SwapTimerBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwapTimerBehaviour : MonoBehaviour
+{
+    [SerializeField] private Text Timer;
+
+    void Update(){
+        Timer.text = ((int)PlayerManager.Instance.swapTimer).ToString();
+    }
+}
